fd7f03e09b4095a1d9b492bfe8e51bd4119891c1
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-09-25 10:15:58 +0800

    added nav drawer

--- youtube-clone/src/components/Menu.vue
+++ youtube-clone/src/components/Menu.vue
@@ -1,47 +1,36 @@
 <template>
-  <v-toolbar flat dark>
-    <v-card class="mx-auto" height="400" width="256">
-      <v-navigation-drawer class="deep-purple accent-4" permanent>
-        <v-list>
-          <v-list-item v-for="item in items" :key="item.title" link>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <template v-slot:append>
-          <div class="pa-2">
-            <v-btn block>Logout</v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
-    </v-card>
+<div>
+    <NavDrawer></NavDrawer>
+  <v-toolbar>
+              <v-btn
+          color="pink"
+          dark
+          @click.stop="drawer = !drawer"
+        >
+          Toggle
+        </v-btn>
     <v-btn>
-      <v-icon>ICON</v-icon>
+      <v-icon>home</v-icon>
       <v-toolbar-title>YouTube</v-toolbar-title>
     </v-btn>
   </v-toolbar>
+</div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import NavDrawer from '@/components/NavDrawer.vue';
 
 export default {
   name: "menubar",
-  components: {},
+  components: {
+      NavDrawer,
+  },
     data () {
         return {
-            items: [
-            { title: 'Dashboard', icon: 'dashboard' },
-            { title: 'Account', icon: 'account_box' },
-            { title: 'Admin', icon: 'gavel' },
-            ],
+            drawer = 0,
         }
     },
     
